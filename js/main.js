f08151db47879382bcb88b7cac3a1abae65d4737
Jakub Sanecki <j.z.sanecki@gmail.com>
2023-04-30 18:44:41 +0200

    nev menu

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ const rightBorderP = document.querySelectorAll(".my-role p")
 const leftBorderP = document.querySelectorAll(".my-goal p")
 const contentUpContainer = document.querySelectorAll(".content-up-container")
 const contentDownContainer = document.querySelectorAll(".content-down-container")
+const logo = document.querySelector(".nav-menu p")
+const navLink = document.querySelectorAll(".nav-menu a")
+const navMenu = document.querySelector(".nav-menu")
+console.log(navLink)
 
 let c = 0 //content section counter
 let m = 0 // mouse wheel blocker
@@ -56,7 +60,11 @@ const nextPage =() =>{
   if(c===0){
   header.classList.add("header-hide") 
   headerAddons.forEach (element => element.classList.add("hide") )
-
+navMenu.style.visibility = "visible"
+navLink.forEach(navLink => {
+  navLink.style.color = "white"
+})
+logo.style.color = "white"
   
   }
   if(currentSection === undefined){
@@ -83,6 +91,10 @@ const nextPage =() =>{
       previousLeftBorderP.classList.remove("appear")
       previousContentUpContainer.classList.remove("content-container-active")
       previousContentDownContainer.classList.remove("content-container-active")
+      if(window.innerWidth>576){
+      navLink.forEach(navLink => {
+        navLink.style.color = "black"})
+logo.style.color = "black"}
     }
     mainSection.style.backgroundColor = (backgroundColor)
     currentSection.classList.add("content-active") 
@@ -113,6 +125,7 @@ if(c===0){
 
   mainSection.style.backgroundColor = ("white") 
   header.classList.remove("header-hide")  
+  navMenu.style.visibility = "hidden"
   headerAddons.forEach (element => element.classList.remove("hide") )
   console.log("powinien pojawić się header")
 }
@@ -141,6 +154,13 @@ if(c>=1){
   let backgroundColor = previousSection.getAttribute("color")
   console.log(backgroundColor)
   mainSection.style.backgroundColor = (backgroundColor)
+
+}
+if(c===1){
+  if(window.innerWidth>576){
+  logo.style.color = "white"
+  navLink.forEach(navLink => {
+    navLink.style.color = "white"})}
 }
 
   }
